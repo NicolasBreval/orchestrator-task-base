305f9bb3728ec17cf42a164347b0bb76355dcae3
nico <nbreval@isotrol.com>
2022-09-28 09:08:53 +0200

    Change MQMessage's dispatch time format in JSON

--- src/main/kotlin/org/nitb/orchestrator2/task/mq/model/MQMessage.kt
+++ src/main/kotlin/org/nitb/orchestrator2/task/mq/model/MQMessage.kt
@@ -9,8 +9,8 @@ import java.util.UUID
 data class MQMessage<T>(
     val message: T?,
     val sender: String,
-    @param:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
-    @get:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
+    @param:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     val dispatchTime: OffsetDateTime,
     val executionId: String = UUID.randomUUID().toString()
 )
