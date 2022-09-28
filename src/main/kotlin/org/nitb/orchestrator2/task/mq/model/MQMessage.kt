60f261b826c870c4b87659bbca2cab330cf524b4
nico <nbreval@isotrol.com>
2022-09-28 08:13:41 +0200

    Add format for MQMessage's dispatchTime

--- src/main/kotlin/org/nitb/orchestrator2/task/mq/model/MQMessage.kt
+++ src/main/kotlin/org/nitb/orchestrator2/task/mq/model/MQMessage.kt
@@ -1,5 +1,6 @@
 package org.nitb.orchestrator2.task.mq.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import io.micronaut.core.annotation.Introspected
 import java.time.LocalDateTime
 import java.util.UUID
@@ -8,6 +9,8 @@ import java.util.UUID
 data class MQMessage<T>(
     val message: T?,
     val sender: String,
+    @param:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-ddTHH:mm:ss.SSSZ")
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-ddTHH:mm:ss.SSSZ")
     val dispatchTime: LocalDateTime,
     val executionId: String = UUID.randomUUID().toString()
 )
