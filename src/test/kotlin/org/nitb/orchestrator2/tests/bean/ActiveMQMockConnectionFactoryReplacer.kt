f1908967153da6297fc618f810a6646fcbab694c
nico <nbreval@isotrol.com>
2022-10-14 07:09:29 +0200

    FIX - Replace ActiveMQ mock connection factory class with a created bean event listener

--- src/test/kotlin/org/nitb/orchestrator2/tests/bean/ActiveMQMockConnectionFactoryReplacer.kt
+++ src/test/kotlin/org/nitb/orchestrator2/tests/bean/ActiveMQMockConnectionFactoryReplacer.kt
@@ -1,20 +1,20 @@
 package org.nitb.orchestrator2.tests.bean
 
-import io.micronaut.context.annotation.Bean
-import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Requires
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+import jakarta.inject.Singleton
 import org.apache.activemq.ActiveMQConnectionFactory
 import org.apache.activemq.broker.BrokerService
 import java.net.ServerSocket
 import javax.jms.ConnectionFactory
 
 @Suppress("UNUSED")
-@Factory
-class ActiveMQMockConnectionFactory {
+@Singleton
+@Requires(property = "micronaut.jms.activemq.classic.enabled", value = "true")
+class ActiveMQMockConnectionFactoryReplacer: BeanCreatedEventListener<ConnectionFactory> {
 
-    @Bean
-    @Requires(property = "micronaut.jms.activemq.classic.enabled", value = "true")
-    fun createConnectionFactory(): ConnectionFactory {
+    override fun onCreated(event: BeanCreatedEvent<ConnectionFactory>): ConnectionFactory {
         val randomPort = ServerSocket(0).let { val localPort = it.localPort; it.close(); localPort }
         val brokerUrl = "tcp://localhost:$randomPort"
 
@@ -27,5 +27,4 @@ class ActiveMQMockConnectionFactory {
         connectionFactory.brokerURL = brokerUrl
         return connectionFactory
     }
-
 }
